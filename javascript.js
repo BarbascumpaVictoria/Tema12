dd719c876d52634ee5588bc2fc18f3197932c5b8
BarbascumpaVictoria <146160959+BarbascumpaVictoria@users.noreply.github.com>
2024-03-04 14:38:05 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var a = 'string';
+var b = 14;
+var c= true;
+var d = null;
+var e = Symbol ;
+console.log("a este " + typeof a + ", " + "b este " + typeof b +", "+ "c este " + typeof c + ", " + "d este " + typeof d +", "+ "e este " + typeof e)
+console.log(b+(++b))
+console.log(b-(++b))
+console.log(b-(--b))
+console.log(b*(++b))
+console.log(b**(--b))
+console.log(b/(++b))
+console.log(b>(++b))
+console.log(b<(++b))
+console.log(b>=(++b))
+console.log(b<=(++b))
+console.log(b!=(++b))
+console.log(b==(--b))
+console.log(e===e)
+
